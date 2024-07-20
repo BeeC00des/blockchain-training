718683ca3246854445461e337ca62c767d5aa044
Bolarinw Ajayi <ajayibcd@gmail.com>
2024-07-20 15:41:36 +0100

    created wallet connected to ui button

---
+++ src/app/config.ts
@@ -0,0 +1,12 @@
+import { http, createConfig } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+
+export const config = createConfig({
+  chains: [mainnet, sepolia],
+  transports: {
+    // [localhost.id]: http(),
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    // [bnb.id]: http(),
+  },
+})
