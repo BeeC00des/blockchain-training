f4674c8e7feff4bb7eaf7f12d4f1a80de095c358
Bolarinw Ajayi <ajayibcd@gmail.com>
2024-07-21 19:23:28 +0100

    added reason for Ens issue

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -2,7 +2,8 @@ import { useAccount, useEnsName } from 'wagmi'
 
 export function Profile() {
   const { address } = useAccount()
-  // const address = '0xf4030DdD79fc7Fd49b25C976C5021D07568B4F91' as `0x${string}` ;
+  // const address = '0xf4030DdD79fc7Fd49b25C97691' as `0x${string}` ;
+  // Get paid to have Ens address for $7 for some years the use address details here.
 
   const { data, error, status } = useEnsName({ address })
   if (status === 'pending') return <div>Loading ENS name</div>
