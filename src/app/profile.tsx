06e5ab04c2984b62d97e9ae7ae2172322a69aaf1
Bolarinw Ajayi <ajayibcd@gmail.com>
2024-07-21 19:13:19 +0100

    removed private address

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,9 +1,9 @@
 import { useAccount, useEnsName } from 'wagmi'
 
 export function Profile() {
-  // const { address } = useAccount()
-  const address = '0xf4030DdD79fc7Fd49b25C976C5021D07568B4F91' as `0x${string}` ;
-  
+  const { address } = useAccount()
+  // const address = '0xf4030DdD79fc7Fd49b25C976C5021D07568B4F91' as `0x${string}` ;
+
   const { data, error, status } = useEnsName({ address })
   if (status === 'pending') return <div>Loading ENS name</div>
   if (status === 'error')
